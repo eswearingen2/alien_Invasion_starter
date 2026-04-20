4f7fe6d7f5bef8337873bc4a90fbc85d402083ba
Ethan <eswearingen2@cscc.edu>
2026-04-19 21:02:44 -0400

    Refactored calc offset and fleet shape

--- alien_fleet.py
+++ alien_fleet.py
@@ -1,52 +1,68 @@
 import pygame
-from alien import Alien
 from typing import TYPE_CHECKING
+
+from alien import Alien
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
-    
+
 class AlienFleet:
 
     def __init__(self, game: 'AlienInvasion'):
         self.game = game
         self.settings = game.settings
         self.fleet = pygame.sprite.Group()
-        self.direction = self.settings.fleet_direction
-        self.fleet_drop_speed = self.settings.fleet_drop_speed
-    
+        self.fleet_direction = self.settings.fleet_direction
+        self.fleet_drop_speed = self.settings.fleet_drop_speed 
+
         self.create_fleet()
 
     def create_fleet(self):
+        #Create the fleet of aliens
         alien_w = self.settings.alien_w
         alien_h = self.settings.alien_h
         screen_w = self.settings.screen_w
         screen_h = self.settings.screen_h
 
+        half_screen = self.settings.screen_h // 2
+        
         fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
 
-        half_screen = self.settings.screen_h//2
-        fleet_horizontal_space = fleet_w * alien_w
         fleet_vertical_space = fleet_h * alien_h
-        x_offset = int((screen_w - fleet_horizontal_space)//2)
-        y_offset = int((half_screen - fleet_vertical_space)//2)
+        fleet_horizontal_space = fleet_w * alien_w
+        x_offset = int((screen_w - fleet_horizontal_space) // 2)
+        y_offset = int((half_screen - fleet_vertical_space) // 2)
+        x_offset, y_offset = self.calculate_offsets(alien_w, alien_h, screen_w, fleet_w, fleet_h)
+
+        self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
+
+    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
         for row in range(fleet_h):
             for col in range(fleet_w):
                 current_x = alien_w * col + x_offset
                 current_y = alien_h * row + y_offset
                 if col % 2 == 0 or row % 2 == 0:
-                    continue
+                  continue
                 self._create_alien(current_x, current_y)
+
+    def calculate_offsets(self, alien_w, alien_h, screen_w, fleet_w, fleet_h):
+        half_screen = self.settings.screen_h // 2
+        fleet_vertical_space = fleet_h * alien_h
+        fleet_horizontal_space = fleet_w * alien_w
+        x_offset = int((screen_w - fleet_horizontal_space) // 2)
+        y_offset = int((half_screen - fleet_vertical_space) // 2)
+        return x_offset,y_offset
 
 
     def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
-        fleet_w = (screen_w//alien_w)
-        fleet_h = ((screen_h / 2)// alien_h)
+        fleet_w = (screen_w // alien_w)
+        fleet_h = ((screen_h/2) // alien_h)
 
         if fleet_w % 2 == 0:
             fleet_w -= 1
-        else: 
+        else:
             fleet_w -= 2
-        
+
         if fleet_h % 2 == 0:
             fleet_h -= 1
         else:            
@@ -59,8 +75,8 @@ class AlienFleet:
         new_alien = Alien(self, current_x, current_y)
 
         self.fleet.add(new_alien)
-    
+
     def draw(self):
-        alien: 'Alien'
+        alien: 'alien'
         for alien in self.fleet:
             alien.draw_alien()
