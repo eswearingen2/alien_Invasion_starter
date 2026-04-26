5ab1fafa11e1d2e4929435ad48a5c4cac6ea6842
Ethan <eswearingen2@cscc.edu>
2026-04-26 15:13:15 -0400

    p3 - finished hud to display lives and levels and plugged into alien invasion game

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,6 +81,7 @@ class AlienInvasion:
             #update game stats for new level
             self.game_stats.update_level()
             #update HUD view
+            self.HUD.update_level()
 
     def _check_game_status(self):
         if self.game_stats.ships_left > 0:
